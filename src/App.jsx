cbc3401ded890cf506ba9fbd8be17657318eee25
mustajab5014-514 <mustajab5014.514@proton.me>
2023-08-24 14:17:34 +0500

    file and folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>App</div>;
-}
+import { Routes, Route } from 'react-router-dom';
+import { Actors, Movies, MovieInformation, Profile } from './pages';
+import { Navbar } from './components';
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" Component={Movies} />
+          <Route path="/movie/:id" Component={MovieInformation} />
+          <Route path="/actor/:id" Component={Actors} />
+          <Route path="/profile/:id" Component={Profile} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Actors } from './Actors';
+export { default as MovieInformation } from './MovieInformation';
+export { default as Movies } from './Movies';
+export { default as Profile } from './Profile';
